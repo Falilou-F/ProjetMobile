15b3b86f0d55ee87eeb8376bed417817bc42d07a
Zaikkker <143960935+Zaikkker@users.noreply.github.com>
2026-03-24 11:26:03 +0100

    Ajout du logo et du design de la page d'accueil

---
+++ SicilyLinesMobile/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SicilyLinesMobile
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
